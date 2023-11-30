a18c235083c2b5c1c8402b822031e20010871ec1
Ishu Kumar Pathak <pishu1766@gmail.com>
2023-11-30 14:52:14 +0530

    map connected using react leaflet, countries autocomplete

--- src/app/components/modals/RentModel.tsx
+++ src/app/components/modals/RentModel.tsx
@@ -6,6 +6,8 @@ import Heading from "../Heading";
 import { categories } from "../Navbar/Categories";
 import CategoryInput from "../Inputs/CategoryInput";
 import { FieldValues, useForm } from "react-hook-form";
+import CountrySelect from "../Inputs/CountrySelect";
+import dynamic from "next/dynamic";
 enum STEPS {
   CATEGORY = 0,
   LOCATION = 1,
@@ -38,6 +40,14 @@ const RentModel = () => {
     },
   });
   const category = watch("category");
+  const location = watch("location");
+  const Map = useMemo(
+    () =>
+      dynamic(() => import('../Map'), {
+        ssr: false
+      }),
+    [location]
+  );
   const setCustomValue = (id: string, value: any) => {
     setValue(id, value, {
       shouldValidate: true,
@@ -65,7 +75,7 @@ const RentModel = () => {
       return undefined;
     }
     return "Back";
-  }, []);
+  }, [step]);
 
   let bodyContent = (
     <div
@@ -90,11 +100,28 @@ const RentModel = () => {
       </div>
     </div>
   );
+
+  if (step === STEPS.LOCATION) {
+    bodyContent = (
+      <div className=" flex flex-col gap-8">
+        <Heading
+          title="Where your place located ?"
+          subtitle="Help resources finds you !"
+        />
+        <CountrySelect
+          value={location}
+          onChange={(value) => setCustomValue("location", value)}
+        />
+        <Map center={location?.latlng} />
+      </div>
+    );
+  }
+
   return (
     <Model
       isOpen={rentModel.isOpen}
       onClose={rentModel.onClose}
-      onSubmit={rentModel.onClose}
+      onSubmit={onNext}
       actionLabel={actionLabel}
       secondaryActionLabel={secondaryActionLabel}
       secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}
